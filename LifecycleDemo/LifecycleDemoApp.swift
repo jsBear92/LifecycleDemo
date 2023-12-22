23ca6a1ef53296e317881c4e353d1ff0150deb3a
Jaeseong Jeong <js104427@gmail.com>
2023-12-22 17:40:28 +0800

    Initial Commit

---
+++ LifecycleDemo/LifecycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleDemoApp.swift
+//  LifecycleDemo
+//
+//  Created by Jaeseong Jeong on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LifecycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
